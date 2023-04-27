7583919bb8ff9988ed1c5dbc7d91b56c6205791c
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-04-27 12:16:10 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,7 @@
 #include "doctest.h"
 #include "sources/Fraction.hpp"
 #include<iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -21,6 +22,7 @@ TEST_CASE("check operator =="){
 
     CHECK((Fraction(1,2) == Fraction(2,4)));
     CHECK_FALSE(f1 == f2);
+    CHECK(0.5 == Fraction(1,2));
 }
 
 TEST_CASE("check reduction "){
@@ -30,14 +32,15 @@ TEST_CASE("check reduction "){
 }
 
 TEST_CASE("check + operator fraction"){
-    Fraction f1(8,9);
+    Fraction f1(6,4);
     Fraction f2(5,2);
 
-    CHECK((f1+f2) == Fraction(53, 18));
+    CHECK((f1+f2) == Fraction(8, 2));
 
 }
 
 TEST_CASE("check  operator - fraction"){
+    
     Fraction f1(7,2);
     Fraction f2(5,2);
     CHECK((f1-f2) == Fraction(2,2));
@@ -105,10 +108,10 @@ TEST_CASE("check the denominator cannot be zero") {
 
 TEST_CASE("check the ++ , ++(int)"){
     Fraction f(1,2);
-
-    CHECK(f++ == Fraction(3,2));
-    
-    CHECK( ++f == Fraction(3,2));
+    f++;
+    CHECK( f == Fraction(3,2));
+    Fraction f1(1,2);
+    CHECK( ++f1 == Fraction(3,2));
 }
 
 TEST_CASE("check the -- , --(int) operators"){
@@ -116,14 +119,14 @@ TEST_CASE("check the -- , --(int) operators"){
 
     f--;
     CHECK(f == Fraction(-1,5));
-    --f;
-    CHECK( f == Fraction(-1,5));
+    Fraction f1(4,5);
+    CHECK( --f1 == Fraction(-1,5));
 }
 
 TEST_CASE("testing input >>"){
     // create an std::istringstream and intilaize it with string
     // then use the operator >> to enter a string into it (fraction represented as string)
-    std:: isstringstream iss("8/9");
+    std::istringstream iss("8/9");
     Fraction f;
     iss >> f;
     CHECK(f == Fraction(8,9));
@@ -135,27 +138,50 @@ TEST_CASE("testing output <<"){
      //create an std::ostringstream object oss, and use the << operator to write a formatted string to it.
      std::ostringstream oss;
      Fraction f(4,5);
-     oos<< f;
-    CHECK (oos.str() == "4/5");
+     oss<< f;
+    CHECK (oss.str() == "4/5");
 
 }
 
-TEST_CASE(""){
-    
+TEST_CASE("check <= , < operators"){
+    Fraction f1(2,6);
+    Fraction f2(3,9);
+
+    CHECK(f1<=f2);
+    CHECK_FALSE(f1<f2);
 }
 
-TEST_CASE(""){
-    
+TEST_CASE("check operator + with numbers"){
+    double num = 2.500;
+    Fraction f(3,5);
+
+    CHECK(num+f == Fraction(3.100) );
 }
 
-TEST_CASE(""){
-    
+TEST_CASE("check operator / with numbers"){
+    double num = 2.000;
+    Fraction f(10/5);
+
+    CHECK(num/f == Fraction(1,1) );
 }
 
-TEST_CASE(""){
-    
+TEST_CASE("check operator * with numbers"){
+    double num = 2.400;
+    Fraction f(3,4);
+
+    CHECK( num*f == Fraction(12,5)*f );
+
 }
 
-TEST_CASE(""){
-    
+TEST_CASE("check operators < ,> , <= , >= with numbers"){
+    double num = 2.400;
+    Fraction f(3,4);
+
+    CHECK_FALSE(f > num);
+    CHECK(f < num);
+    CHECK_FALSE(f >= num);
+    CHECK_FALSE(num < f);
+    CHECK(num > f);
+    CHECK(num >= f);
+    CHECK(f <= num);
 }
