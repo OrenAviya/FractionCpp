eb60383032b5e21e6a92b7714626be51c202a9ca
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-05-01 20:05:34 +0300

    tests and demo working good

--- sorces/Fraction.cpp
+++ sorces/Fraction.cpp
@@ -9,13 +9,21 @@ using namespace ariel;
         }    
 
         Fraction:: Fraction(int numerator, int denominator)/*num(numerator) , den(denominator)*/ {
-            if ( denominator == 0)
+            if ( denominator == 0 && numerator!=0 )
                 throw std :: invalid_argument ("This demoninator is illegal");
-            
+           
+        
             //reduce the fraction
             int gcf = gcd(numerator, denominator); // find greatest common factor
             numerator /= gcf;
             denominator /= gcf;
+
+            //Avoid problems with equals fractions...
+            if (denominator < 0 ){
+                numerator *=-1;
+                denominator *= -1;
+            }
+
             this->num = numerator;
             this->den = denominator;
         }
@@ -28,43 +36,54 @@ using namespace ariel;
         numerator /= gcf;
         denominator /= gcf;
 
+        //Avoid problems with equals fractions...
+            if (denominator < 0 ){
+                numerator *=-1;
+                denominator *= -1;
+            }
+            
         this->num = numerator;
         this->den = denominator ;      
         }
         
     //getters & setters
 
-        void Fraction::setNum(int n){
+        void Fraction::setNumerator(int n){
             this->num = n;
         }
-        void Fraction::setDen(int d){
-            if (d == 0.0)
+        void Fraction::setDenominator(int d){
+            if (d == 0.0 || d== 0)
                 throw std :: invalid_argument ("This demoninator is illegal");
             
             this->den = d;
         }
 
-        int Fraction::getNum() const{
+        int Fraction::getNumerator() const{
             return this->num;
         }
-        int Fraction:: getDen() const{
+        int Fraction:: getDenominator() const{
             return this->den;
         }
         
 
 
-        Fraction Fraction:: operator+(Fraction f){
-        int denominator = this->den * f.getDen();
-        int numerator = ((f.getDen() * this->num) +(this->den * f.getNum())) ;
-        Fraction frac=Fraction(numerator, denominator); //reducing by constructor
-        return frac;
+        Fraction Fraction:: operator+(Fraction frac){
+        int denominator = this->den * frac.getDenominator();
+        int numerator ;
+        // checking if the operation couses an overflow: 
+        if (__builtin_add_overflow((frac.getDenominator() * this->num), (this->den * frac.getNumerator()), &numerator))
+        {
+            throw std::overflow_error("Overflow error: result is too large");
+        }
+        Fraction frac1=Fraction(numerator, denominator); //reducing by constructor
+        return frac1;
         }
 
-        const Fraction operator+(double num , const Fraction f){
+         Fraction operator+(double num , const Fraction &frac){
             Fraction temp(num);
-            Fraction frac= (temp+f);
-            frac.reduce();
-            return (frac);
+            Fraction frac1= (temp+frac);
+            frac1.reduce();
+            return (frac1);
         }
 
         Fraction Fraction:: operator+(double num){
@@ -74,13 +93,18 @@ using namespace ariel;
             return (frac);
         }
 
-        Fraction Fraction:: operator-(Fraction f){
+        Fraction Fraction:: operator-(Fraction frac){
         
-        int denominator = this->den * f.getDen();
-        int numerator = (this->num * f.getDen()) - (f.getNum() * this->den) ;
-        Fraction frac=Fraction(numerator, denominator);
-        frac.reduce();
-        return frac;
+        int denominator = this->den * frac.getDenominator();
+        int numerator;
+        // checking if the operation couses an overflow
+        if (__builtin_sub_overflow((this->num * frac.getDenominator()), (frac.getNumerator() * this->den), &numerator))
+        {
+        throw std::overflow_error("Overflow error: result is too large");
+        }
+        Fraction frac1=Fraction(numerator, denominator);
+        frac1.reduce();
+        return frac1;
         }
 
         Fraction Fraction:: operator-(double num){
@@ -91,44 +115,69 @@ using namespace ariel;
         }
 
 
-        const Fraction operator-(double num , const Fraction f){
+         Fraction operator-(double num , const Fraction &frac){
             Fraction temp(num);
-            return (temp-f);
+            return (temp-frac);
         }
 
-        Fraction Fraction:: operator/(Fraction f){
-            int numerator = this->num * f.getDen();
-            int denominator = this->den * f.getNum();
-            Fraction frac = Fraction(numerator , denominator);
-             frac.reduce();
-             return frac;
+        Fraction Fraction:: operator/(Fraction frac){
+            int numerator;
+            int denominator;
+            // checking if the operation couses an overflow:
+            if (__builtin_mul_overflow(this->num ,  frac.getDenominator(), &numerator))
+            {
+            throw std::overflow_error("Overflow error: result is too large");
+            }
+            if (__builtin_mul_overflow(this->den ,  frac.getNumerator(), &denominator))
+            {
+            throw std::overflow_error("Overflow error: result is too large");
+            }
+            
+            if(denominator == 0 )
+            {
+            throw std :: runtime_error ("cannot dividing by zero");
+            }
+            
+            Fraction frac1 = Fraction(numerator , denominator);
+             frac1.reduce();
+             return frac1;
         }
 
-        const Fraction operator/(double num , const Fraction f){
+         Fraction operator/(double num , const Fraction &frac){
             Fraction temp(num);
-            return (temp/f);
+            return (temp/frac);
         }
 
         Fraction Fraction:: operator/(double number) {
             if (number == 0.0){
-                throw std :: invalid_argument ("devision by zero is illegal");
+                throw std :: runtime_error("devision by zero is illegal");
             }
             //make fraction from the number , then use / with two fractions
             Fraction f(number); 
             return *this / f; 
 
         }
-        Fraction Fraction:: operator*(Fraction f){
-            int numerator = this->num * f.getNum();
-           int denominator = this->den * f.getDen();
-          Fraction frac = Fraction(numerator , denominator);
-          frac.reduce();
-            return frac;
+        Fraction Fraction:: operator*(Fraction frac){
+            int numerator ;
+           int denominator;
+           // checking if the operation couses an overflow:
+           if (__builtin_mul_overflow(this->num ,  frac.getNumerator(), &numerator))
+            {
+            throw std::overflow_error("Overflow error: result is too large");
+            }
+            if (__builtin_mul_overflow(this->den ,  frac.getDenominator(), &denominator))
+            {
+            throw std::overflow_error("Overflow error: result is too large");
+            }
+
+          Fraction frac1 = Fraction(numerator , denominator);
+          frac1.reduce();
+            return frac1;
         }
-        const Fraction operator*(double num , const Fraction f){
-            Fraction frac = Fraction(num);
-            frac.reduce();
-            return frac*f;
+         Fraction operator*(double num , const Fraction &frac){
+            Fraction frac1 = Fraction(num);
+            frac1.reduce();
+            return frac1*frac;
         }
         Fraction Fraction:: operator*(double number){
             Fraction temp(number);
@@ -159,8 +208,8 @@ using namespace ariel;
         }
 
 
-        bool Fraction :: operator>=(const Fraction& f) const{
-            if (*this > f || *this == f ){
+        bool Fraction :: operator>=(const Fraction& frac) const{
+            if (*this > frac || *this == frac ){
                 return true;
             } 
             return false;
@@ -170,53 +219,55 @@ using namespace ariel;
             return (*this > number || *this == number );
          }
 
-        const bool operator>=(double num , const Fraction f){
+         bool operator>=(double num , const Fraction &frac){
             Fraction temp(num);
-            return (temp >= f);
+            return (temp >= frac);
         }
 
-        bool Fraction ::operator <=(const Fraction &f) const{
-            return (*this < f || *this == f);
+        bool Fraction ::operator <=(const Fraction &frac) const{
+            return (*this < frac || *this == frac);
                 
         }
         bool Fraction ::operator<=(double number) const{
             Fraction temp(number);
             return (*this <= temp);
         }
-        const bool operator<=(double num , const Fraction f){
+         bool operator<=(double num , const Fraction &frac){
             Fraction temp(num);
-            return (temp <= f);
+            return (temp <= frac);
         }
 
-        bool Fraction :: operator <(const Fraction &f) const{
-            return ((double)(this->num * f.getDen()) < (double)(f.getNum() * this->den)); 
+        bool Fraction :: operator <(const Fraction &frac) const{
+            return ((double)(this->num * frac.getDenominator()) < (double)(frac.getNumerator() * this->den)); 
         }
          bool Fraction :: operator < (double number) const{
             Fraction temp(number);
             return (*this < temp);
          }
-         const bool operator<(double num , const Fraction f){
+          bool operator<(double num , const Fraction &frac){
             Fraction temp(num);
-            return (temp < f);
+            return (temp < frac);
         }
 
         bool Fraction:: operator >(float number) const{
             return(this -> num > (number* this->den));
         }
         
-        bool Fraction::operator >(const Fraction &f) const{
-             return ((double)(this->num * f.getDen()) > (double)(f.getNum() * this->den));
+        bool Fraction::operator >(const Fraction &frac) const{
+             return ((double)(this->num * frac.getDenominator()) > (double)(frac.getNumerator() * this->den));
+             
         }
 
-        const bool operator >(double number , const Fraction f){
+         bool operator >(double number , const Fraction &frac){
             Fraction temp(number);
-            return (temp > f);
+            return (temp > frac);
         }
 
-        bool Fraction:: operator==(const Fraction& f) const{
-            // to be sure they both have only three numbers after point
+        bool Fraction:: operator==(const Fraction& frac) const{
+          
+             // to be sure they both have only three numbers after point
             double f1 = std :: round ((double)(this->num)/ this->den*1000)/1000.0;
-            double f2 = std :: round ((double)(f.getNum()) / f.getDen()*1000)/ 1000.0; 
+            double f2 = std :: round ((double)(frac.getNumerator()) / frac.getDenominator()*1000)/ 1000.0; 
 
             return (f1 == f2);
         }
@@ -225,24 +276,34 @@ using namespace ariel;
            return (number == *this);
            
         }
-        const bool operator ==(double num , const Fraction f){
-            return (Fraction(num) == f);
+         bool operator ==(double num , const Fraction &frac){
+            return (Fraction(num) == frac);
         }
 
-        std::ostream& operator<<(std::ostream& output, const Fraction& f)
+        std::ostream& operator<<(std::ostream& output, const Fraction& frac)
         {
-        return output << f.getNum()<< "/" << f.getDen();
+             if(frac.getDenominator()< 0 || frac.getNumerator() < 0) // check if numerator or denominator is negative
+    { // to show an union print to all fractions:
+        int n = abs(frac.getNumerator()); 
+        int d = abs(frac.getDenominator()); 
+        output << "-"; // print a minus sign
+        output << n << "/" << d;
+    }
+    else{
+        output << frac.getNumerator()<< "/" << frac.getDenominator();
+    }
+        return output;
         }
 
-        std::istream& operator>>(std::istream& input, Fraction& f){
+        std::istream& operator>>(std::istream& input, Fraction& frac){
         char c ;
         int num , den;
          
         input >> num >> den ;
         
         if (input && den != 0) {
-        f.setNum(num);
-        f.setDen(den);
+        frac.setNumerator(num);
+        frac.setDenominator(den);
     }
      else {
         throw runtime_error("Bad_in");
@@ -252,10 +313,10 @@ using namespace ariel;
 
 
    // for reduction     
-int Fraction::gcd(int a, int b) { // function to find greatest common divisor
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+int Fraction::gcd(int one, int two) { // function to find greatest common divisor
+    if (two == 0)
+        return one;
+    return gcd(two, one % two);
 }
 
 void Fraction:: reduce(){
--- sorces/Fraction.hpp
+++ sorces/Fraction.hpp
@@ -16,29 +16,29 @@ class Fraction{
 
     //getters & setters
 
-        void setNum(int);
-        void setDen(int);
+        void setNumerator(int);
+        void setDenominator(int);
 
-        int getNum() const;
-        int getDen() const;
+        int getNumerator() const;
+        int getDenominator() const;
         
     // operators
-        Fraction operator+(Fraction f);
+     
+        Fraction operator+(Fraction frac);
         Fraction operator+(double num);
-        friend const Fraction operator+(double num , const Fraction f);
+        // friend const Fraction operator+(double num , const Fraction frac);
 
-        Fraction operator-(Fraction f);
+        Fraction operator-(Fraction frac);
         Fraction operator-(double num);
-        // Fraction operator-(int num);
-        friend const Fraction operator-(double num , const Fraction f);
+        // friend const Fraction operator-(double num , const Fraction frac);
 
-        Fraction operator/(Fraction f);
+        Fraction operator/(Fraction frac);
         Fraction operator/(double number);
-        friend const Fraction operator/(double num , const Fraction f);
+        // friend const Fraction operator/(double num , const Fraction frac);
 
-        Fraction operator*(Fraction f);
+        Fraction operator*(Fraction frac);
         Fraction operator*(double number);
-        friend const Fraction operator*(double num , const Fraction f);
+        // friend const Fraction operator*(double num , const Fraction frac);
         
         Fraction& operator++();
     
@@ -48,39 +48,36 @@ class Fraction{
     
         Fraction operator--(int);
 
-        bool operator>=(const Fraction &f) const;
+        bool operator>=(const Fraction &frac) const;
         bool operator>=(double number) const ;
-        friend const bool operator>=(double num , const Fraction f);
         
-        bool operator <=(const Fraction &f) const;
+        bool operator <=(const Fraction &frac) const;
         bool operator<=(double number) const ;
-        friend const bool operator<=(double num , const Fraction f);
 
-        bool operator <(const Fraction &f) const;
+        bool operator <(const Fraction &frac) const;
         bool operator < (double number) const;
-        friend const bool operator <(double num , const Fraction f);
-
-        bool operator >(const Fraction &f) const;
+        
+        bool operator >(const Fraction &frac) const;
         bool operator>(float number) const;
-        friend const bool operator >(double number , const Fraction f);
-
+       
         bool operator==(const Fraction& other) const;
         bool operator == (double number)const;
-        friend const bool operator ==(double num , const Fraction f);
-
-        friend std::ostream& operator<<(std::ostream& out, const Fraction& fraction);
-    
-        friend std::istream& operator>>(std::istream& in, Fraction& fraction);
-
-        int gcd (int a , int b);
+        
+        int gcd (int one , int two);
         void reduce();
 
 
 };
+//all the "friend" function but we want them "const" and it is imposible so did it out of class...
+std::ostream& operator<<(std::ostream& out, const Fraction& fraction);   
+std::istream& operator>>(std::istream& input, Fraction& fraction);
+ bool operator ==(double num , const Fraction &frac);
+ bool operator >(double number , const Fraction &frac);
+ bool operator <(double num , const Fraction &frac);
+ bool operator<=(double num , const Fraction &frac);
+ bool operator>=(double num , const Fraction &frac);
+Fraction operator*(double num , const Fraction &frac);
+Fraction operator/(double num , const Fraction &frac);
+Fraction operator-(double num , const Fraction &frac);
+Fraction operator+(double num , const Fraction &frac);
 
-    
-   
-    
-   
-   
-    
